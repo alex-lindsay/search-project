2e374b123f081cd27dfdb8412914eaa84b05cf83
Alex Lindsay <work@alex-lindsay.com>
2020-04-18 15:49:11 -0700

    Connect app to AWS Cognito

---
+++ reactui/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  //   s3: {
+  //     REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+  //     BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME",
+  //   },
+  //   apiGateway: {
+  //     REGION: "YOUR_API_GATEWAY_REGION",
+  //     URL: "YOUR_API_GATEWAY_URL",
+  //   },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_jxYFoumAK",
+    APP_CLIENT_ID: "14r7cocv02l2entl9eou39nm0s",
+    IDENTITY_POOL_ID: "us-west-2:f12d40e2-fb39-4a7a-b751-37c91ed3227a",
+  },
+};
