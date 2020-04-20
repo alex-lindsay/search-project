5c389fc9d845d4de94cfba2424a8cac4d4380081
Alex Lindsay <work@alex-lindsay.com>
2020-04-20 09:09:13 -0700

    Cleanup environments to access API server

--- reactui/src/containers/Products/Products.js
+++ reactui/src/containers/Products/Products.js
@@ -1,29 +1,27 @@
-import React, { useState } from "react";
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import "./Products.css";
 
 const Products = (props) => {
-  const [products] = useState();
-  // const [products, setProducts] = useState();
+  const [products, setProducts] = useState();
 
-  // useEffect(() => {
-  //   async function fetchProducts() {
-  //     console.log("let's get some products");
-  //     try {
-  //       const result = await axios.get(
-  //         `http://searchproducts.alex-lindsay.com/api/products/products`
-  //       );
-  //       console.log("after axios", result.data);
-  //       setProducts(result.data);
-  //     } catch (error) {
-  //       console.warn("TODO REPLACE THIS WITH AN ERROR DISPATCH");
-  //       console.error("Error getting products:", error);
-  //     }
-  //   }
-  //   fetchProducts();
-  // }, []);
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        console.log(process.env);
+        const result = await axios.get(
+          `${process.env.REACT_APP_API_URL_PRODUCTS}/products`
+        );
+        console.log("after axios", result.data);
+        setProducts(result.data);
+      } catch (error) {
+        console.warn("TODO REPLACE THIS WITH AN ERROR DISPATCH");
+        console.error("Error getting products:", error);
+      }
+    }
+    fetchProducts();
+  }, []);
 
   const productRows = products ? (
     products.map((product) => <p>product.name</p>)
